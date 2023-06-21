8a17777980da230913fc142c4fd03c6f28b99c4e
huewen <huyenjijung@gmail.com>
2023-06-21 14:09:19 +0700

    QLCaFe

---
+++ prj2/project2/frmTroGiup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace project2
+{
+    public partial class frmTroGiup : Form
+    {
+        public frmTroGiup()
+        {
+            InitializeComponent();
+        }
+
+        private void btthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmTroGiup_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
